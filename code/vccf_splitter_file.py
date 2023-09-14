b57e46ddfb11bbe52752b9259037a52eba49d72e
juyingnan <bunny_gg@hotmail.com>
2023-09-14 03:19:17 -0700

    updated due to the new version data

--- code/vccf_splitter_file.py
+++ code/vccf_splitter_file.py
@@ -8,7 +8,7 @@ Output: 64 .csv files will be generated based on 'unique_region' coulumn in inpu
 """""
 
 # Give path to your data from source : https://www.biorxiv.org/content/10.1101/2021.11.25.469203v1 (High Resolution Single Cell Maps Reveals Distinct Cell Organization and Function Across Different Regions of the Human Intestine)
-split_source_file = r'G:\HuBMAP\Hickey\doi_10.5061_dryad.pk0p2ngrf__v5/CODEX_HuBMAP_alldata_Dryad.csv'
+split_source_file = r'G:\HuBMAP\Hickey\doi_10.5061_dryad.pk0p2ngrf__v11/23_09_CODEX_HuBMAP_alldata_Dryad_merged.csv'
 
 data_new = pd.read_csv(split_source_file)
 
@@ -32,8 +32,10 @@ for label in unique_regions:
     df_label = data_new[data_new[column_to_split] == label]
 
     # Define target File name and define path to target file
-    split_target_file = f"{split_source_file.replace('doi_10.5061_dryad.pk0p2ngrf__v5/CODEX_HuBMAP_alldata_Dryad.csv', 'Intestine_64_data/Region')}_{i}.csv"
+    split_target_file = f"{split_source_file.replace('doi_10.5061_dryad.pk0p2ngrf__v11/23_09_CODEX_HuBMAP_alldata_Dryad_merged.csv', 'Intestine_64_data/Region')}_{i}.csv"
 
     # Write to the file using pandas to_csv
     df_label.to_csv(split_target_file, index=False, header=True, mode='a')
     i = i +1
+    # print the progress
+    print(f"File {split_target_file} written successfully")
