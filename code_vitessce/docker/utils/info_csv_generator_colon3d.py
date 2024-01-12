db82ffde58b9818788e805d447885a8852fee87d
juyingnan <bunny_gg@hotmail.com>
2024-01-11 17:21:15 -0800

    fixed region names

--- code_vitessce/docker/utils/info_csv_generator_colon3d.py
+++ code_vitessce/docker/utils/info_csv_generator_colon3d.py
@@ -1,12 +1,11 @@
 import os
 import pandas as pd
 
-# Given data
-image_urls = [
-    rf'https://storagetuzi.blob.core.windows.net/blobtuzi/colon_3d/Region_CRC01050_mask.pyramid.ome.tif']
-
-region_names = [f"CRC01050"]
+region_names = ['CRC01002','CRC01007','CRC01014','CRC01020','CRC01025','CRC01029','CRC01034','CRC01039','CRC01044','CRC01049','CRC01050','CRC01051','CRC01052','CRC01054','CRC01059','CRC01064','CRC01069','CRC01074','CRC01078','CRC01084','CRC01086','CRC01091','CRC01097','CRC01102','CRC01106']
 rows = len(region_names)
+
+# Given data
+image_urls = [rf'https://storagetuzi.blob.core.windows.net/blobtuzi/colon_3d/Region_{region}_mask.pyramid.ome.tif' for region in region_names]
 
 # Extract image names from the URLs using os.path.basename
 image_names = [os.path.basename(url) for url in image_urls]
